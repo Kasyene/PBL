ae6d712c65ec06cb2f3a8eafd3ebb9f13d4c2316
203946 Mirkiewicz Kinga <203946@edu.p.lodz.pl>
2018-03-04 16:32:29 +0100

    Prototyp base

---
+++ Prototyp/Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody body;
+    private float distanceToGround;
+    private RaycastHit hit;
+    float x;
+    float z;
+    float h;
+    // Use this for initialization
+    void Start ()
+    {
+        body = GetComponentInChildren<Rigidbody>();
+        distanceToGround = GetComponentInChildren<CapsuleCollider>().bounds.extents.y;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0f, Input.GetAxis("Mouse X") * Time.deltaTime * 50f, 0f);
+        transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * 5f;
+        transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * 5f;
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            body.velocity += new Vector3(0f, 5.0f, 0f);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(body.transform.position, -Vector3.up, distanceToGround + 0.1f);
+    }
+}
