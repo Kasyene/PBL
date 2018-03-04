3f06ead09006d76f5dafc278130b03ef99796436
203946 Mirkiewicz Kinga <203946@edu.p.lodz.pl>
2018-03-04 17:55:58 +0100

    Enemies losing hp when hit by weapon, destroyed after losing all hp

---
+++ Prototyp/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int hp = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	    if (this.hp == 0)
+	    {
+            Destroy(gameObject);
+	    }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "MainWeapon")
+        {
+            this.hp = this.hp - 1;
+        }
+    }
+}
