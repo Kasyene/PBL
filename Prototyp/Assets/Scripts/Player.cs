2f8a216a2ed86bab03f29a98ca43dadee6750b78
Kinga Mirkiewicz <kinga.mirkiewicz@gmail.com>
2018-03-15 21:18:35 +0100

    Base jump combos -> to be changes, do not use

--- Prototyp/Assets/Scripts/Player.cs
+++ Prototyp/Assets/Scripts/Player.cs
@@ -17,9 +17,7 @@ public class Player : MonoBehaviour {
     private  float lastClickedTime = 0.0f;
     private  float maxComboDelay = 1.5f;
     private float minComboDelay = 0.2f;
-    float x;
-    float z;
-    float h;
+    private float lastJumpTime = 0.0f;
 
 
 
@@ -35,6 +33,9 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        Debug.Log(animator.GetBool("basicAttack1") + " " + animator.GetBool("basicAttack2")
+                   +" " + animator.GetBool("basicAttack3") + " " + animator.GetBool("rangedAttack")
+        +" " + animator.GetBool("jumpAttack1") + " " + animator.GetBool("jumpAttack2"));
         if ((Time.time - lastClickedTime > maxComboDelay))
         {
             numberOfClicks = 0;
@@ -49,18 +50,38 @@ public class Player : MonoBehaviour {
         // skok
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
+            lastJumpTime = Time.time;
             body.velocity += new Vector3(0f, 5.0f, 0f);
         }
 
-        // ataki
+        // basic attack + basic attack jump combo
         if (Input.GetMouseButtonDown(0))
         {
-            basicAttack();
+            if (Time.time - lastJumpTime < 0.5f)
+            {
+                if (!IsAnyAttackBoolTrue())
+                {
+                    animator.SetBool("jumpAttack1", true);
+                }
+            }
+            else
+            {
+                basicAttack();
+            }
+           
         }
 
+        // ranged attack + ranged attack jump combo
         if (Input.GetMouseButtonDown(1) && !IsAnyAttackBoolTrue())
         {
-            animator.SetBool("rangedAttack", true);
+            if (Time.time - lastJumpTime < 0.5f)
+            {
+                animator.SetBool("jumpAttack2", true);
+            }
+            else
+            {
+                animator.SetBool("rangedAttack", true);
+            } 
         }
 
 
@@ -91,24 +112,26 @@ public class Player : MonoBehaviour {
         numberOfClicks = Mathf.Clamp(numberOfClicks, 0, 3);
     }
 
-    bool IsGrounded()
-    {
-        return Physics.Raycast(body.transform.position, -Vector3.up, distanceToGround + 0.1f);
-    }
-    bool AnimatorIsPlaying()
-    {
-        return animator.GetCurrentAnimatorStateInfo(0).length >
-               animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
-    }
-
     bool IsAnyAttackBoolTrue()
     {
         if (animator.GetBool("basicAttack1") || animator.GetBool("basicAttack2")
-            || animator.GetBool("basicAttack3") || animator.GetBool("rangedAttack"))
+            || animator.GetBool("basicAttack3") || animator.GetBool("rangedAttack")
+            || animator.GetBool("jumpAttack1") || animator.GetBool("jumpAttack2"))
         {
             return true;
         }
         return false;
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(body.transform.position, -Vector3.up, distanceToGround + 0.1f);
+    }
+
+    bool AnimatorIsPlaying()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).length >
+               animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
     }
 
     bool AnimatorIsPlaying(string stateName)
