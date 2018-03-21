770dd41454145fa2380245beb934af46126cc7ae
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-03-21 18:26:13 +0100

    No attack queue

---
+++ Prototyp/Assets/Scripts/AnimatorScripts/Idle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("jumpAttack1", false);
+        animator.SetBool("jumpAttack2", false);
+        animator.SetBool("basicAttack1", false);
+        animator.SetBool("basicAttack2", false);
+        animator.SetBool("basicAttack3", false);
+        animator.SetBool("rangedAttack", false);
+
+    }
+
+}
