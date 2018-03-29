960479d75d8ea2e5580ce3b8ea4295425267dac6
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-03-29 17:19:33 +0200

    Transform class + SceneNode(not work, WIP)

---
+++ Game/Game/SceneGraph/SceneNode.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Game.SceneGraph
+{
+    class SceneNode
+    {
+        private SceneNode parent = null;
+        private Transformation transform = new Transformation();
+        private Matrix localTransform = Matrix.Identity;
+        private Matrix worldTransform = Matrix.Identity;
+        private List<SceneNode> childs = new List<SceneNode>();
+
+        private bool isDirty = true; //true if Transformation change
+
+        internal SceneNode Parent
+        {
+            get
+            {
+                return parent;
+            }
+        }
+
+        public SceneNode()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+            //UpdateTransformation();
+
+            foreach (SceneNode node in childs)
+            {
+                node.Draw();
+            }
+        }
+
+        public void AddChildNode(SceneNode node)
+        {
+            if (node.parent != null)
+            {
+                throw new System.Exception("Can't add a node that already have a parent.");
+            }
+            childs.Add(node);
+
+            node.SetParent(this);
+        }
+
+        public void RemoveChildNode(SceneNode node)
+        {
+            if (node.parent != this)
+            {
+                throw new System.Exception("Can't remove a node that don't belong to this parent.");
+            }
+            childs.Remove(node);
+
+            node.SetParent(null);
+        }
+
+        public void RemoveFromParent()
+        {
+            if (parent == null)
+            {
+                throw new System.Exception("Can't remove an orphan node from parent.");
+            }
+            parent.RemoveChildNode(this);
+        }
+
+        protected virtual void SetParent(SceneNode newParent)
+        {
+            parent = newParent;
+        }
+
+        protected virtual void UpdateTransformations()
+        {
+            if (isDirty)
+            {
+                localTransform = transform.CreateMatrix();
+                if (parent != null)
+                {
+                    worldTransform = localTransform * parent.worldTransform;
+                }
+                else
+                {
+                    worldTransform = localTransform;
+                }
+            }
+            isDirty = false;
+        }
+    }
+}
---
+++ Game/Game/SceneGraph/Transformation.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game.SceneGraph
+{
+    public enum TransformationOrder
+    {
+        PositionRotationScale,
+        PositionScaleRotation,
+        ScalePositionRotation,
+        ScaleRotationPosition,
+        RotationScalePosition,
+        RotationPositionScale,
+    }
+
+    public enum RotationOrder
+    {
+        RotateXYZ,
+        RotateXZY,
+        RotateYXZ,
+        RotateYZX,
+        RotateZXY,
+        RotateZYX,
+    }
+
+    class Transformation
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+        public Vector3 scale;
+
+        public TransformationOrder transformOrder = TransformationOrder.ScaleRotationPosition;
+        public RotationOrder rotationOrder = RotationOrder.RotateYXZ;
+
+        public Transformation()
+        {
+            position = Vector3.Zero;
+            rotation = Vector3.Zero;
+            scl = Vector3.One;
+        }
+
+        public Transformation(Transformation other)
+        {
+            position = other.position;
+            rotation = other.rotation;
+            scl = other.scl;
+            transformOrder = other.transformOrder;
+            rotationOrder = other.rotationOrder;
+        }
+
+        public Matrix CreateRotationMatrix()
+        {
+            switch (rotationOrder)
+            {
+                case RotationOrder.RotateXYZ:
+                    return Matrix.CreateRotationX(rotation.X) * Matrix.CreateRotationY(rotation.Y) * Matrix.CreateRotationZ(rotation.Z);
+
+                case RotationOrder.RotateXZY:
+                    return Matrix.CreateRotationX(rotation.X) * Matrix.CreateRotationZ(rotation.Z) * Matrix.CreateRotationY(rotation.Y);
+
+                case RotationOrder.RotateYXZ:
+                    return Matrix.CreateRotationX(rotation.Y) * Matrix.CreateRotationZ(rotation.X) * Matrix.CreateRotationY(rotation.Z);
+
+                case RotationOrder.RotateYZX:
+                    return Matrix.CreateRotationY(rotation.Y) * Matrix.CreateRotationZ(rotation.Z) * Matrix.CreateRotationX(rotation.X);
+
+                case RotationOrder.RotateZXY:
+                    return Matrix.CreateRotationZ(rotation.Z) * Matrix.CreateRotationX(rotation.X) * Matrix.CreateRotationY(rotation.Y);
+
+                case RotationOrder.RotateZYX:
+                    return Matrix.CreateRotationZ(rotation.Z) * Matrix.CreateRotationY(rotation.Y) * Matrix.CreateRotationX(rotation.X);
+
+                default:
+                    throw new System.Exception("RotationOrder not exist");
+            }
+        }
+
+        public Matrix CreateMatrix()
+        {
+            Matrix pos = Matrix.CreateTranslation(position);
+            Matrix rot = CreateRotationMatrix();
+            Matrix scl = Matrix.CreateScale(scale);
+
+            switch (transformOrder)
+            {
+                case TransformationOrder.PositionRotationScale:
+                    return pos * rot * scl;
+
+                case TransformationOrder.PositionScaleRotation:
+                    return pos * scl * rot;
+
+                case TransformationOrder.ScalePositionRotation:
+                    return scl * pos * rot;
+
+                case TransformationOrder.ScaleRotationPosition:
+                    return scl * rot * pos;
+
+                case TransformationOrder.RotationScalePosition:
+                    return rot * scl * pos;
+
+                case TransformationOrder.RotationPositionScale:
+                    return rot * pos * scl;
+
+                default:
+                    throw new System.Exception("TransformationOrder not exist");
+            }
+        }
+    }
+}
+
+}
