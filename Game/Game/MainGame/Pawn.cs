9d5918b4cea6c80e9db45c8e670d7dfa4a520fdb
Kinga Mirkiewicz <kinga.mirkiewicz@gmail.com>
2018-04-03 01:44:29 +0200

    Empty pawn and player created

---
+++ Game/Game/MainGame/Pawn.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using PBLGame.SceneGraph;
+
+namespace PBLGame.MainGame
+{
+    class Pawn : ModelEntity
+    {
+        public Pawn(Model loadedeModel) : base(loadedeModel)
+        {
+            //TODO, too sleepy right now :P
+        }
+    }
+}
---
+++ Game/Game/MainGame/Player.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace PBLGame.MainGame
+{
+    class Player : Pawn
+    {
+        public Player(Model loadedeModel) : base(loadedeModel)
+        {
+            //TODO, too sleepy right now :P
+        }
+    }
+}
