ca4cd5f96f7ef927e51bcdea4792cfbac77321d7
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-04-02 14:03:42 +0200

    Calculation of BoundingBox

--- Game/Game/SceneGraph/ModelEntity.cs
+++ Game/Game/SceneGraph/ModelEntity.cs
@@ -48,7 +48,29 @@ namespace PBLGame.SceneGraph
 
         public BoundingBox GetBoundingBox(SceneNode parent, Matrix localTransformations, Matrix worldTransformations)
         {
-            throw new NotImplementedException();
+            Vector3 min = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+            Vector3 max = new Vector3(float.MinValue, float.MinValue, float.MinValue);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                {
+                    int vertexStride = meshPart.VertexBuffer.VertexDeclaration.VertexStride;
+                    int vertexBufferSize = meshPart.NumVertices * vertexStride;
+
+                    float[] vertexData = new float[vertexBufferSize / sizeof(float)];
+                    meshPart.VertexBuffer.GetData<float>(vertexData);
+                
+                    for (int i = 0; i < vertexBufferSize / sizeof(float); i += vertexStride / sizeof(float))
+                    {
+                        Vector3 currPosition = new Vector3(vertexData[i], vertexData[i + 1], vertexData[i + 2]);
+                        currPosition = Vector3.Transform(currPosition, worldTransformations);
+                        min = Vector3.Min(min, currPosition);
+                        max = Vector3.Max(max, currPosition);
+                    }
+                }
+            }
+            return new BoundingBox(min, max);
         }
     }
 }
