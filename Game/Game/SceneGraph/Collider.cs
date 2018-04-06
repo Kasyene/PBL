e4a142081742d32f5e4c9b6ea600e1d483a10159
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-04-06 14:28:24 +0200

    Collider class

---
+++ Game/Game/SceneGraph/Collider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBLGame.SceneGraph
+{
+    class Collider
+    {
+        protected BoundingBox boundingBox;
+        public bool isTrigger;
+
+        public Collider(BoundingBox bounding)
+        {
+            boundingBox = bounding;
+        }
+
+        public void SetBoundingBox(BoundingBox bounding)
+        {
+            boundingBox = bounding;
+        }
+
+        public bool IsCollision(BoundingBox other)
+        {
+            if (boundingBox.Intersects(other)) return true;
+            return false;
+        }
+    }
+}
