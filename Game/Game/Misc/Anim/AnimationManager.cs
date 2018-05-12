6a88008005721d50cdd194d1434a3f8ed9067437
Unknown <marvin77@op.pl>
2018-05-12 16:55:39 +0200

    Animation Manager - work in progress

---
+++ Game/Game/Misc/Anim/AnimationManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using AnimationAux;
+using Microsoft.Xna.Framework;
+using PBLGame.SceneGraph;
+using Component = PBLGame.SceneGraph.Component;
+
+namespace PBLGame.Misc.Anim
+{
+    public class AnimationManager : Component
+    {
+        private GameObject parent;
+
+        private AnimationPlayer animationPlayer;
+
+        private AnimationClip animationClip;
+
+        private Dictionary<string, AnimationClip> animationClips = new Dictionary<string, AnimationClip>();
+
+        public void PlayAnimation(string _key)
+        {
+            animationClip = animationClips[_key];
+        }
+
+        public void AddAnimation(AnimationClip _animClip, string _key)
+        {
+            animationClips.Add(_key,_animClip);
+        }
+
+        public Dictionary<string, AnimationClip> GetAnimationDictionary()
+        {
+            return animationClips;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (!ReferenceEquals(animationClip,animationPlayer.Clip))
+            {
+                animationPlayer = parent.GetComponent<ModelAnimatedComponent>().PlayClip(animationClip);
+            }
+            base.Update(gameTime);
+        }
+
+        AnimationManager(GameObject _parent)
+        {
+            parent = _parent;
+        }
+
+        AnimationManager(Dictionary<string, AnimationClip> _animationClips)
+        {
+            animationClips = _animationClips;
+        }
+    }
+}
