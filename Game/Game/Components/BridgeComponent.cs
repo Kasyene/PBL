e9267fc33c11d3d19f9760a2fa4f1a71eabff748
Unknown <marvin77@op.pl>
2018-06-01 20:11:00 +0200

    Missing files

---
+++ Game/Game/Components/BridgeComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using PBLGame.SceneGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Components
+{
+    class BridgeComponent : Component
+    {
+        GameObject parent;
+        public bool dropBridge = true;
+
+        public BridgeComponent(GameObject _parent)
+        {
+            parent = _parent;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if(dropBridge)
+            {
+                parent.UnSetModelQuat();
+                parent.RotationZ += 0.2f * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (parent.RotationZ > 1.5f)
+                {
+                    dropBridge = false;
+                }
+            }
+        }
+    }
+}
